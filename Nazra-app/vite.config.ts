af0495a6eda7a960cf3d415db8669954a784f924
Zain Ali <zainaliworkmail3830@gmail.com>
2026-07-20 00:47:34 +0500

    change the base URL

--- Nazra-app/vite.config.ts
+++ Nazra-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nazra-app/',
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
